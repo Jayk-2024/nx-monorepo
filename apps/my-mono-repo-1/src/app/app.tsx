8d5a6d877893438513e2635fc705bd5b85604bd5
Jayk <jay.khavadia@rlogical.com>
2025-01-30 11:58:15 +0530

    demo monorepo TS

--- apps/my-mono-repo-1/src/app/app.tsx
+++ apps/my-mono-repo-1/src/app/app.tsx
@@ -1,11 +1,32 @@
-import NxWelcome from './nx-welcome';
+// import NxWelcome from './nx-welcome';
 
-export function App() {
+// export function App() {
+//   return (
+//     <div>
+//       <NxWelcome title="my-mono-repo-1" />
+//     </div>
+//   );
+// }
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import { Products } from '@my-nx-monorepo/products';
+import NxWelcome from './nx-welcome';
+function App() {
   return (
-    <div>
-      <NxWelcome title="my-mono-repo-1" />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/nx" element={<NxWelcome title="my-mono-repo-1" />} />
+        {/* Add more routes as needed */}
+      </Routes>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home Page</h2>;
 }
 
 export default App;
---
+++ libs/products/src/lib/products.tsx
@@ -0,0 +1,10 @@
+// import styles from './products.module.css';
+export function Products() {
+  return (
+    <div className="container">
+      <h1>Welcome to Products!</h1>
+    </div>
+  );
+}
+
+export default Products;
